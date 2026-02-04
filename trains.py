5b90e2fa1f8a9e1fda2f1cf906df465d259eb138
Georgi Angelov <gtangelov76@gmail.com>
2026-02-04 18:14:08 +0200

    Update trains.py

--- trains.py
+++ trains.py
@@ -1,3 +1,14 @@
 number_of_trains = [0] * int(input())
-print(number_of_trains)
 
+while True:
+    command = input().split()
+    current_command = command[0]
+    people = int(command[2])
+    if current_command == "add":
+        number_of_trains[::-1] += int(command[1])
+    elif current_command == "insert":
+        number_of_trains.insert(int(command[1]), people)
+    elif current_command == "leave":
+        number_of_trains[int(command[1])] -= people
+    elif command == "End":
+        print("")
